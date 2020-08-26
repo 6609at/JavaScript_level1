d830e6ceb9bf7dd354a5733ad8ddb59cd9990afb
Aleksandr <Siodmf@gmail.com>
2020-08-26 15:21:13 +0300

    inWorkHW

--- Day5/Mission_1/script.js
+++ Day5/Mission_1/script.js
@@ -1,16 +1,50 @@
-var chessWrap = document.querySelector('.ch-wrap');
+//создаем блок в который поместим еще один блок для доски
+var Wrap = document.createElement('div');
+Wrap.classList.add('top-wrap');
+document.body.appendChild(Wrap);
 
-var i = 0, count = 0;
 
-while (count < 8 * 8) {
-  var item = document.createElement('div');
-  chessWrap.appendChild(item);
 
-  item.classList.add('ch-item');
 
-  if (i && i % 2)
-    item.classList.add('ch-black')
+function generateChess() {
 
-  i += ((i + 2) % 9) ? 1 : 2;
-  count++;
+  //создаем блок для самой доски
+  var chessWrap = document.createElement('div');
+  Wrap.appendChild(chessWrap);
+  chessWrap.classList.add('ch-wrap');
+  let i = 0, count = 0;
+  while (count < 8 * 8) {
+    var item = document.createElement('div');
+    chessWrap.appendChild(item);
+    item.classList.add('ch-item');
+
+    if (i && i % 2)
+      item.classList.add('ch-black')
+    i += ((i + 2) % 9) ? 1 : 2;
+    count++;
+  }
+
+  
+  
 }
+
+function chessboard() {
+  let i = 0;
+  
+  let blackFigs1 = [ '&#9820;','&#9822;','&#9821;','&#9819;','&#9818;','&#9821;','&#9822;','&#9820;'];
+  let whiteFigs1 = ['&#9814;','&#9816;','&#9815;','&#9813;','&#9812;','&#9821;','&#9815;','&#9814;'];
+  let blackFigs2 = ['&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;'];
+  let whiteFigs2 = ['&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;'];
+
+  let elem = document.querySelectorAll('.ch-item');
+  for ( let a = 0; a < 11; a++) {
+    
+    document.querySelector('.ch-item').innerHTML += '1';
+  }
+  
+  
+
+  
+
+}
+
