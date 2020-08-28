f1afe50668e744119a1f90e04ae42d3dd290ee16
Aleksandr <Siodmf@gmail.com>
2020-08-28 11:10:26 +0300

    Finall version HW

--- Day5/Mission_1/script.js
+++ Day5/Mission_1/script.js
@@ -37,23 +37,23 @@ function generateChess() {
     count++;
   }
   bot();
-
-
-
 }
 
 
 function chessboard() {
+  //тут убираем кнопку что бы на ее месте сделать другую
   let delBtn2 = document.querySelector('#BtnSecond');
   delBtn2.remove();
-
+//тут выбираем все наши div которые создали ранее. 
+//вся наша доска это просто 64 div. Вот их мы и выбрали
   let elem = document.getElementsByClassName('ch-item');
+  //простые массивы для вигур
   let blackFigs1 = ['&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;', '&#9821;', '&#9822;', '&#9820;'];
   var blackFigs2 = ['&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;'];
   let whiteFigs2 = ['&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;'];
   let whiteFigs1 = ['&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9821;', '&#9815;', '&#9814;'];
-  /* console.log(elem); */
-
+  
+  //расставляем наши фигуры из массива по полю
   for (let i = 0; i < 8; i++) {
     /* console.log(blackFigs1[i]); */
     elem[i].innerHTML = blackFigs1[i];
@@ -69,44 +69,34 @@ function chessboard() {
     elem[i].innerHTML = whiteFigs1[i - 56];
   }
 }
-
+//вынес в отдельную функцию создание div под буквы и цифры
 var bot = function bot() {
   //создаем див для текста доски сниззу
   let textBottomWrp = document.createElement('div');
   Wrap.appendChild(textBottomWrp);
   textBottomWrp.classList.add('tx-bot-wr');
-  function txBot() {
-    let elemBot = document.getElementsByClassName('tx-bot-wr');
-  } 
-
+  let textBottomTx = document.querySelector('.tx-bot-wr');
+  textBottomTx.innerHTML = 'A B C D E F G H';
+ 
   //создаем div для текста доски сверху
   let textTopWrp = document.createElement('div');
   Wrap.prepend(textTopWrp);
   textTopWrp.classList.add('tx-top-wr');
+  let textTopTx = document.querySelector('.tx-top-wr');
+  textTopTx.innerHTML = 'A B C D E F G H';
 
   //создаем div для цифр доски слева
   let numLeftWrp = document.createElement('div');
   Wrap.prepend(numLeftWrp);
   numLeftWrp.classList.add('nm-left-wr');
+  let textLeftTx = document.querySelector('.nm-left-wr');
+  textLeftTx.innerHTML = '1 2 3 4 5 6 7 8';
 
   //создаем div для цифр доски слева
   let numRigWrp = document.createElement('div');
   Wrap.prepend(numRigWrp);
   numRigWrp.classList.add('nm-right-wr');
-
-
-
+  let textRigTx = document.querySelector('.nm-right-wr');
+  textRigTx.innerHTML = '1 2 3 4 5 6 7 8';
 }
 
-/* let b = 0;
-let textBot = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-let elemb = document.getElementsByClassName('tx-bot-wr');
-while (b < 8) {
-  var item2 = document.createElement('div');
-  textBottomWrp.appendChild(item2);
-  item2.classList.add('tx-bot-item');
-  b++
-}
-for (let i = 0; i < 8; i++) {
-  elemb[i].innerHTML = textBot[i];
-} */
